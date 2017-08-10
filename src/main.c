fd2d332b5da8ceaa971778ddc134f684fd9e70b6
lomassubedi <subedilomas@gmail.com>
2017-08-11 02:27:32 +0545

    Done 1K vars.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 
 uint16_t hr, mn, sec;
 uint16_t testEnumP1, testEnumP2, testEnumP3, testEnumP5, testEnumP6, testEnumP7;
+uint16_t testEnumP10, testEnumP11 , testEnumP12, testEnumP13, testEnumP14, testEnumP15, testEnumP16;
+uint16_t testEnum17, testEnum18, testEnum19, testEnum20, testEnum21, testEnum22, testEnum23, testEnum24;
+uint8_t testEnum25, testEnum26;
 
 int main(void)
 {	
@@ -20,6 +23,23 @@ int main(void)
 		testEnumP6 = Ad1_Pr2_FXP2_Tmr_DC_CclTime;
 		testEnumP7 = Ad1_Pr3_HdV_Tmr_LC_CclTime;
 		
+		testEnumP10 = Ad1_Pr3_Cir_Htr_LC_OffTemp;				
+		testEnumP11 = Ad1_Pr3_Ven_HumVnt_DC_OnHum;
+		testEnumP12 = Ad1_Pr3_FXP2_FlexFunc;
+		testEnumP13 = Ad1_Pr4_HdV_DC_SrtDly;
+		testEnumP14 = Ad1_Pr4_Cir_Tmr_DC_CclTime;
+		testEnumP15 = Ad1_Pr4_Ven_RptVnt_DC_RptAftr;
+		testEnumP16 = Ad1_Pr4_FXP2_LC_OnOff;
 		
+		testEnum17 = Ad1_Pr5_Light_LC_TimeRemain;
+		testEnum18 = Ad1_Pr5_Cir_VentLoc_YesNo;
+		testEnum19 = Ad1_Pr5_Ven_LC_HumVnt_OffHum;
+		testEnum20 = Ad1_Pr5_FXP1_DeH_DC_OffHum;
+		testEnum21 = Ad1_Pr6_Light_OffTime;
+		testEnum22 = Ad1_Pr6_Cir_LC_OnOff;
+		testEnum23 = Ad1_Pr6_Ven_TempVnt_LC_OnTemp;
+		testEnum24 = Ad1_Pr6_FXP1_Hum_DC_OffHum;
+		testEnum25 = Ad1_Pr6_FXP2_Co2_CylGen;
+		testEnum26 = Ad1_Pr7_HdV_DeH_DC_OnHum;
   }
 }
