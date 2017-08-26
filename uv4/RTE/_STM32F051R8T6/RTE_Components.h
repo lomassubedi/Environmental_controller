d57fd14cc80de8860b638e591512154769612447
lomassubedi <subedilomas@gmail.com>
2017-08-26 21:58:20 +0545

    Timer2 configured.

---
+++ uv4/RTE/_STM32F051R8T6/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ENV_CTRL_F051_REV0' 
+ * Target:  'STM32F051R8T6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
