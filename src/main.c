62ad3df5142956e5bc1db734c75fb972517f0b37
lomassubedi <subedilomas@gmail.com>
2017-09-17 12:57:27 +0545

    RTC LSE.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "stm32f0xx.h"
 #include "stm32f0xx_it.h"
 
@@ -6,6 +7,7 @@
 #include "stm32f0xx_gpio.h"
 #include "stm32f0xx_usart.h"
 #include "stm32f0xx_rcc.h"
+#include "stm32f0xx_rtc.h"
 
 #include "flags_timers.h"
 #include "reg_list.h"
@@ -13,9 +15,10 @@
 
 #include "modbus_slave.h"
 
+#define 		TMP_BUFFER_SIZE			200
+uint8_t buffr[TMP_BUFFER_SIZE];
+
 GPIO_InitTypeDef GPIO_InitStructure;
-
-
 
 static __IO uint32_t TimingDelay;
 
@@ -33,19 +36,121 @@ extern void TimingDelay_Decrement(void) {
   }
 }
 
-int main(void) {		
+static void RTC_Config(void) {
+	
+  RTC_InitTypeDef  RTC_InitStructure;
+  RTC_TimeTypeDef  RTC_TimeStruct;
+
+  /* Enable the PWR clock */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+
+  /* Allow access to RTC */
+//  PWR_BackupAccessCmd(ENABLE);
+
+  /* Reset RTC Domain */
+  RCC_BackupResetCmd(ENABLE);
+  RCC_BackupResetCmd(DISABLE);
+
+  /* Enable the LSE OSC */
+  RCC_LSEConfig(RCC_LSE_ON);
+
+  /* Wait till LSE is ready */  
+  while(RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET);
+
+  /* Select the RTC Clock Source */
+  RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
+
+  /* Configure the RTC data register and RTC prescaler */
+  /* ck_spre(1Hz) = RTCCLK(LSI) /(AsynchPrediv + 1)*(SynchPrediv + 1)*/
+  RTC_InitStructure.RTC_AsynchPrediv = 0x7F;
+  RTC_InitStructure.RTC_SynchPrediv  = 0xFF;
+  RTC_InitStructure.RTC_HourFormat   = RTC_HourFormat_24;
+  RTC_Init(&RTC_InitStructure);
+  
+  /* Set the time to 00h 00mn 00s AM */
+  RTC_TimeStruct.RTC_H12     = RTC_H12_AM;
+  RTC_TimeStruct.RTC_Hours   = 0x00;
+  RTC_TimeStruct.RTC_Minutes = 0x00;
+  RTC_TimeStruct.RTC_Seconds = 0x00;  
+  RTC_SetTime(RTC_Format_BCD, &RTC_TimeStruct);
+	
+	RTC_TimeStructInit(&RTC_TimeStruct); 
+}
+
+void init_usart2(void) {
+//	/ USART periferial initialization settings
+	  USART_InitTypeDef USART_InitStructure;
+	  GPIO_InitTypeDef	GPIO_InitStructure;
+
+	  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+	  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_1);
+	  GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_1);
+
+	  //Configure USART2 pins: TX (PA2) and RX (PA3)
+	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	  //Configure USART2 setting: ----------------------------
+	  USART_InitStructure.USART_BaudRate = 9600;
+	  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	  USART_InitStructure.USART_Parity = USART_Parity_No;
+	  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	  USART_Init(USART2, &USART_InitStructure);
+	  USART_Cmd(USART2,ENABLE);
+}
+
+void usart2_putchar(uint8_t c) {
+	while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+	USART_SendData(USART2, c);
+	return;
+}
+
+void usart2_puts(uint8_t *str) {
+	while(*str) {
+		usart2_putchar(*str);
+		str++;
+	}
+	return;
+}
+
+
+int main(void) {
+	
+	RTC_TimeTypeDef myRTCTime;
+	RTC_DateTypeDef myRTCDate;
+	
 	SysTick_Config(SystemCoreClock / 1000);
 	
 	STM_EVAL_LEDInit(LED3);
 	STM_EVAL_LEDInit(LED4);
 	
+	init_usart2();
 	init_modbus();
+	
+	RTC_Config();
+	
+  while (1) {
 		
-  while (1) {			
 		if(flagLEDIndi) {
 			flagLEDIndi = 0;
 			STM_EVAL_LEDToggle(LED3);
 		}
-		modbus_update();
+//		modbus_update();
+		RTC_GetTime(RTC_Format_BCD, &myRTCTime);
+		sprintf((char *)buffr, "Hour: %d\tMinute: %d\tSec: %d\r\n", myRTCTime.RTC_Hours, myRTCTime.RTC_Minutes, myRTCTime.RTC_Seconds);
+		usart2_puts(buffr);
+		
+		RTC_GetDate(RTC_Format_BCD, &myRTCDate);
+		sprintf((char *)buffr, "Year: %d\tMonths: %d\tDay: %d\r\n", myRTCDate.RTC_Year, myRTCDate.RTC_Month, myRTCDate.RTC_Date);
+		usart2_puts(buffr);
+	
+		Delay(1000);
 	}
 }
